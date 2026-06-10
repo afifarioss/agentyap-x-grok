efb67bbb9a7767ea560056683db7a825604f78da
Afifarioss <afif.peugeot@gmail.com>
2026-06-10 23:27:21 +0800

    Fix layout build error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,4 @@
 import type { Metadata } from "next";
-import "./globals.css";
-import { AuthKitProvider } from "@farcaster/auth-kit";
 
 export const metadata: Metadata = {
   title: "AgentYap",
@@ -18,20 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <meta name="fc:frame" content="vNext" />
-        <meta name="fc:miniapp" content="true" />
-      </head>
-      <body>
-        <AuthKitProvider
-          config={{
-            domain: "agentyap-x-grok.vercel.app", // tukar kepada domain kau
-            siweUri: "https://agentyap-x-grok.vercel.app",
-          }}
-        >
-          {children}
-        </AuthKitProvider>
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
