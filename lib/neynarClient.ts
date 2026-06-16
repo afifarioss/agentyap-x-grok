11661e1cbd3bd1d2fdca59259a775e47bcd1e29b
afifarioss.base.eth <afif.peugeot@gmail.com>
2026-06-16 15:23:41 +0800

    Create lib/neynarClient.ts

---
+++ lib/neynarClient.ts
@@ -0,0 +1,13 @@
+import { Configuration, NeynarAPIClient } from "@neynar/nodejs-sdk";
+
+if (!process.env.NEYNAR_API_KEY) {
+  throw new Error("NEYNAR_API_KEY is missing");
+}
+
+const config = new Configuration({
+  apiKey: process.env.NEYNAR_API_KEY,
+});
+
+const neynarClient = new NeynarAPIClient(config);
+
+export default neynarClient;
