de6acdde65eb1050e64bac764d4117324c8f83b1
afifarioss.base.eth <afif.peugeot@gmail.com>
2026-06-16 14:44:09 +0800

    Create utils/getFid.ts

---
+++ utils/getFid.ts
@@ -0,0 +1,19 @@
+import neynarClient from "@/lib/neynarClient";
+import { mnemonicToAccount } from "viem/accounts";
+
+export const getFid = async () => {
+  if (!process.env.FARCASTER_DEVELOPER_MNEMONIC) {
+    throw new Error("FARCASTER_DEVELOPER_MNEMONIC is not set.");
+  }
+
+  const account = mnemonicToAccount(
+    process.env.FARCASTER_DEVELOPER_MNEMONIC as `${string} ${string} ${string}`
+  );
+
+  const { user: farcasterDeveloper } =
+    await neynarClient.lookupUserByCustodyAddress({
+      custodyAddress: account.address,
+    });
+
+  return Number(farcasterDeveloper.fid);
+};
