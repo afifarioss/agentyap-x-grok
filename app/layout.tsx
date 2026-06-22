51174469d0097a6060b6a15c97836517f9caa530
afifarioss.base.eth <afif.peugeot@gmail.com>
2026-06-23 01:27:00 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,12 +4,13 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "AgentYap",
-  description: "Grok-powered Autonomous Farcaster Agent on Base",
+  description: "AI-powered Autonomous Farcaster Agent on Base",
   icons: {
     icon: "/favicon.ico",
   },
   other: {
     "talentapp:project_verification": "a876db4829bb43594f7d522243aab73d43fb206e5c3f1d2de908bd21ea1df4cddbd149c9fed0fec6644b43679b4b8b288a0e54fd9c41fa55fbca384a9b473978",
+    "base:app_id": "6a396c1230074aac8d95099e",
   },
 };
 
